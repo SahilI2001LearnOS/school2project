24402a8255d2232740eb9f85aaa6a1a6a65f4c05
Varun Bhati <varun.bhati@ongraph.com>
2024-01-25 14:57:26 +0530

    Initial Commit

---
+++ school2project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  school2project
+//
+//  Created by OnGraph Technologies on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
